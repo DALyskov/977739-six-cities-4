f45d9960b32a75a56085ce8084a79354b67fa707
DALyskov <DALyskov@gmail.com>
2020-07-04 22:19:51 +0300

    Выполнил задание 2.2

--- src/components/main/main.test.js
+++ src/components/main/main.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import renderer from 'react-test-renderer';
 
-import App from './components/app/app.jsx';
+import Main from './main.jsx';
 
-const placeData = [
+const placeDataTest = [
   {
     id: 1,
     isPremium: true,
@@ -36,4 +36,22 @@ const placeData = [
   },
 ];
 
-ReactDOM.render(<App placeData={placeData} />, document.querySelector(`#root`));
+describe(`Main snapchots`, () => {
+  it(`with data`, () => {
+    const tree = renderer
+      .create(
+        <Main placeData={placeDataTest} onPlaceCardNameClick={() => {}} />
+      )
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it(`without data`, () => {
+    const tree = renderer
+      .create(<Main placeData={[]} onPlaceCardNameClick={() => {}} />)
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
+
+export {placeDataTest};
